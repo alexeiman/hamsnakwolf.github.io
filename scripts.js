cdaf773b5f233f047b7de9f2c5242a2a1c06c095
Alexei Pino <alexeipinogruber@gmail.com>
2024-11-08 20:36:43 -0400

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,11 @@
+function LanguageDropdown()
+{
+    if (document.getElementById("Droppie").style.display == "block")
+        {
+            document.getElementById("Droppie").style.display = "none";
+        }
+        else
+        {
+            document.getElementById("Droppie").style.display = "block";
+        }
+}
